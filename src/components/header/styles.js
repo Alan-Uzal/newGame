188dd08048fc4140ce9f6cbd965f2892effb7d06
Alan Uzal <uzal.alan@gmail.com>
2022-11-25 19:59:14 -0300

    Creando estructura de mi primer juego

---
+++ src/components/header/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles=StyleSheet.create({
+    container:{
+        backgroundColor: "#071013",
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop:30,
+    },
+    title:{
+        fontSize:22,
+        color: "#dfe0e2",
+        fontWeight:"bold",
+    }
+});
